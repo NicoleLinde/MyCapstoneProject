01195d0d35eb0cf0ec301431f9b1a0636b7b4316
Nicole Linde <Collchen@outlook.de>
2021-04-08 16:39:25 +0200

    styled Start-Links

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import Bee from '../assets/bee.jpg'
+import Butterfly from '../assets/butterfly.jpg'
+import Bird from '../assets/gimpel.jpg'
 
 export default function Links() {
     return (
         <Wrapper>
             <StyledLink to='/Bee'><Img src={Bee} />Biene</StyledLink>
-            <StyledLink to='/Butterfly'>Falter</StyledLink>
-            <StyledLink to='/Bird'>Vogel</StyledLink>
+            <StyledLink to='/Butterfly'><Img src={Butterfly} />Falter</StyledLink>
+            <StyledLink to='/Bird'><Img src={Bird} />Vogel</StyledLink>
         </Wrapper>
     )
 
@@ -18,23 +20,24 @@ height: 100vh;
 display: flex;
 flex-direction:column;
 align-items:  center;
-margin-top:2rem;
+margin-top:1.5rem;
 `
 
 const StyledLink = styled(Link)`
 height: 6rem;
 width: 15rem;
-padding: 1rem;
+padding: 1rem 0.5rem;
 display: flex;
 align-items: center;
-gap: 2rem;
-  box-shadow:9px 13px 40px var(--primary);
+gap: 3rem;
+  box-shadow:8px 12px 30px var(--primary);
   border-radius: 1rem;
   margin: 1rem 1rem 1rem 1rem;
   background-color: white;
   position: relative;
   text-decoration: none;
   color: #246914;
+ font-size: x-large;
  
 `
 
@@ -42,5 +45,5 @@ const Img = styled.img`
 width: 6rem;
 height: 5rem;
 border-radius: 1rem;
+box-shadow:1px 1px 10px var(--primary);
 `
-
